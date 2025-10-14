a8a802f584248acfb5b9fd8cf3c5ff980dba9cdc
winston93-cloud <sistemas.desarrollo@winston93.edu.mx>
2025-10-14 11:11:22 -0600

    Deshabilitar cache para rutas de confirmación

--- next.config.js
+++ next.config.js
@@ -3,7 +3,29 @@ const nextConfig = {
   output: 'standalone',
   images: {
     unoptimized: true
-  }
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/confirmar/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0',
+          },
+        ],
+      },
+      {
+        source: '/confirmar-v2/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
