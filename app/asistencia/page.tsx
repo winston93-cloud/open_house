42b3527915ad4193d115cdb63095a42f6f6c69dc
winston93-cloud <sistemas.desarrollo@winston93.edu.mx>
2025-10-14 12:06:03 -0600

    Forzar regeneración de bundle para /asistencia

--- app/asistencia/page.tsx
+++ app/asistencia/page.tsx
@@ -3,6 +3,8 @@
 import { useState, useEffect, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 
+// Force bundle regeneration
+
 interface Inscripcion {
   id: string;
   nombre_aspirante: string;
